28f591a79effd797e52bdbebfd9926b11309a455
songh <songwuxin@live.cn>
2018-04-28 22:44:34 +0800

    demo.sh Please enter the commit message for your changes. Lines starting

---
+++ demo.sh
@@ -0,0 +1,12 @@
+mkdir $1
+cd $1
+mkdir css js
+touch index.html css/style.css js/main.js
+echo '<!DOCTYPE>' > index.html
+echo '<title>Hello</title>' >> index.html
+echo '<h1>Hi</h1>' >> index.html
+echo 'h1{color: red;}' > css/style.css
+echo 'var string = "Hello World"' > js/main.js
+echo 'alert(string)' >> js/main.js
+exit
+
