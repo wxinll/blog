81c1a835ce4a5fbd814915fa9e33689a472fb427
songwx <songwuxin@live.cn>
2018-06-02 23:27:58 +0800

    jQuery

--- slideShow/jQuery.js
+++ slideShow/jQuery.js
@@ -1,13 +1,17 @@
 var $spans = $('.index > span')
+
 var $images = $('.images img')
-var $index = $('.index')
+
 for (let i = 0; i < $spans.length; i++) {
 	let p = i * -500
+
 	$($spans[i]).on('click', (x) => {
 		var index = $(x.currentTarget).index()
+
 		$images.css({
 			transform: 'translateX(' + p + 'px)'
 		})
+
 		$spans.eq(index)
 			.addClass('red')
 			.siblings('.red').removeClass('red')
@@ -16,16 +20,20 @@ for (let i = 0; i < $spans.length; i++) {
 
 var n = 0
 var size = $spans.length
-var timer = setInterval(()=> {
-	n += 1
-	$spans.eq(n % 3).trigger('click')
-},1000)
 
-$('.images').on('mouseover', ()=> clearInterval(timer))
+var timer = setTimer()
 
-$('.images').on('mouseleave', ()=> {
-	timer = setInterval(()=> {
-	n += 1
-	$spans.eq(n % 3).trigger('click')
-},1000)
+$('.window').on('mouseover', ()=> {
+	window.clearInterval(timer)
 })
+
+$('.window').on('mouseleave', ()=> {
+	timer = setTimer()
+})
+
+function setTimer() {
+	return setInterval(() => {
+		n += 1
+		$spans.eq(n % size).trigger('click')
+	}, 1000)
+}
