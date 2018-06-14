a781167e06d835f42d8f05eb6cfa559a415b01e4
songwx <songwuxin@live.cn>
2018-06-14 23:22:07 +0800

    model view

--- CV/messages.js
+++ CV/messages.js
@@ -11,61 +11,64 @@
 		fetch: function() {
 			var query = new AV.Query('messages')
 			return query.find()
-				.then(function(objects) {
-					objects.forEach(function(value) {
-						var name = value.attributes.name
-						var content = value.attributes.content
-					})
-				}, function(error) {
-					// 异常处理
-				}) //Promise
 		},
-		save: function() {
+		save: function(name,content) {
 			var Message = AV.Object.extend('messages');
 			var message = new Message();
 			return message.save({
-				name: name,
-				content: content
+				'name': name,
+				'content': content
 			})
 		},
 	}
 
-	var view = ''
+	var view = document.querySelector('.messages')
 
 	var controller = {
 		model: null,
 		view: null,
 		init:function(model,view){
+			this.view = view
 			this.model = model
-			this.model.fetch()
+			this.model.init()
 			this.loadMessage()
 			this.bindEvents()
 		},
 
 		loadMessage: function() {
-			let li = document.createElement('li')
-			li.textContent = `${name}:${content}`
-			let messagesLi = document.querySelector('.messages ol')
-			messagesLi.appendChild(li)
-		}
-		bindEvents: function(){
-			($('.messages form')).on('submit', (e) => {
-				e.preventDefault()
-				let name = $('form input[name=name]')[0].value
-				let content = $('form input[name=content]')[0].value
+			this.model.fetch().then(function(objects) {
+				objects.forEach(function(value) {
+					let name = value.attributes.name
+					let content = value.attributes.content
+					let li = document.createElement('li')
+					li.textContent = `${name}:${content}`
+					let messagesLi = document.querySelector('.messages ol')
+					messagesLi.appendChild(li)
+				})
+			}, function(error) {
+				// 异常处理
+			}) 
+		},
+		saveMessage: function() {
+			let name = this.view.querySelector('input[name=name]').value
+			let content = this.view.querySelector('input[name=content]').value
+			this.model.save(name, content).then( (objects)=> {
 				let li = document.createElement('li')
 				li.textContent = `${name}:${content}`
-				let messagesLi = document.querySelector('.messages ol')
+				let messagesLi = this.view.querySelector('ol')
 				messagesLi.appendChild(li)
-
-				this.model.save()
-
-				$('form input[name=name]')[0].value = ''
-				$('form input[name=content]')[0].value = ''
+			})
+		},
+		bindEvents: function() {
+			($('.messages form')).on('submit', (e) => {
+				e.preventDefault()
+				this.saveMessage()
+				this.view.querySelector('input[name=name]').value = ''
+				this.view.querySelector('input[name=content]').value = ''
 			})
 		}
 	}
 
-	controller.init()
+	controller.init(model,view)
 
 }.call()
